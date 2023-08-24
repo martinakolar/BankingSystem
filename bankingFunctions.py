31d645a282820757b284052b8b1af6f1cc31abb3
Martina Kolar <martina.kolar7@gmail.com>
2023-08-24 17:27:53 +0200

    added: try except while loop for when the initial deposit input is invalid

--- bankingFunctions.py
+++ bankingFunctions.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+
+#*for the login function
 def catchOtherInfo(username):
     with open("userData.txt", "r") as file:
         lines = file.readlines()
@@ -17,6 +19,9 @@ def catchOtherInfo(username):
     return date_of_birth, home_address, initial_deposit
 
 
+
+
+#*for the banking function
 def typeOfMoneyTransfer():
     while True:
         type = input("Would you like to deposit (1) or withdraw (2)? ")
@@ -31,6 +36,7 @@ def typeOfMoneyTransfer():
             continue
 
 
+
 def appendingToTransactionHistory(transaction_history, type, amount):
     # datetime object containing current date and time
     now = datetime.now()
@@ -40,3 +46,59 @@ def appendingToTransactionHistory(transaction_history, type, amount):
 
     # appending an item to the transaction history
     transaction_history[date_and_time] = f"{type}, €{amount}"
+
+
+
+
+#*for the registration function
+def checkingIfUserAlreadyRegistered(username):
+        with open("userData.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                data = line.strip().split("|")
+                if data[0] == username:
+                    return True
+            else:
+                return False
+
+
+
+def getDateOfBirth(username):
+    print("Date of birth: ")
+
+    day_of_birth = int(input("Day(DD): "))
+    while day_of_birth < 1 or day_of_birth > 31:
+        print("Day of birth must be between 1 and 31!")
+        day_of_birth = int(input("Day(DD): "))
+
+    month_of_birth = int(input("Month(MM): "))
+    while month_of_birth < 1 or month_of_birth > 12:
+        print("Month of birth must be between 1 and 12!")
+        month_of_birth = int(input("Month(MM): "))
+
+    year_of_birth = int(input("Year(YYYY): "))
+    while year_of_birth < 1900 or year_of_birth > 2023:
+        print("Year of birth must be between 1900 and 2023!")
+        year_of_birth = int(input("Year(YYYY): "))
+
+
+    date_of_birth = f"{day_of_birth}.{month_of_birth}.{year_of_birth}"
+    return date_of_birth
+
+
+
+def inputtingInitialDeposit():
+    initial_deposit_bool = input("\nDo you wish to make an initial deposit? (y/n): ").lower() 
+    if initial_deposit_bool == "y":
+        while True:
+            try:
+                initial_deposit_amount = float(input("Enter your initial deposit amount: "))
+            except ValueError:
+                print("Please enter a valid initial deposit amount.")
+                continue
+            else:
+                print("Initial deposit amount successfully made.")
+                return initial_deposit_amount
+    else:
+        initial_deposit_amount = 0
+        return initial_deposit_amount
--- userRegister.py
+++ userRegister.py
@@ -1,4 +1,6 @@
 import bcrypt
+from bankingFunctions import checkingIfUserAlreadyRegistered, getDateOfBirth, inputtingInitialDeposit 
+
 
 def registration():
     print("SIGN UP")
@@ -43,62 +45,11 @@ def registration():
     home_address = input("Home address: ")
 
     #*initial deposit
-    initial_deposit = str(inputtingInitialDeposit())
+    initial_deposit = inputtingInitialDeposit()
 
     with open("userData.txt", "a") as file:
-        file.write(username + "|" + hashed_password.decode('utf-8') + "|" + home_address + "|" + date_of_birth + "|" + initial_deposit + "\n")
+        file.write(username + "|" + hashed_password.decode('utf-8') + "|" + home_address + "|" + date_of_birth + "|" + str(initial_deposit) + "\n")
         print("Registration successful!")
     file.close()
 
     return username, date_of_birth, home_address, initial_deposit
-
-
-
-
-def checkingIfUserAlreadyRegistered(username):
-        with open("userData.txt", "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                data = line.strip().split("|")
-                if data[0] == username:
-                    return True
-            else:
-                return False
-
-
-
-
-def getDateOfBirth(username):
-    print("Date of birth: ")
-
-    day_of_birth = int(input("Day(DD): "))
-    while day_of_birth < 1 or day_of_birth > 31:
-        print("Day of birth must be between 1 and 31!")
-        day_of_birth = int(input("Day(DD): "))
-
-    month_of_birth = int(input("Month(MM): "))
-    while month_of_birth < 1 or month_of_birth > 12:
-        print("Month of birth must be between 1 and 12!")
-        month_of_birth = int(input("Month(MM): "))
-
-    year_of_birth = int(input("Year(YYYY): "))
-    while year_of_birth < 1900 or year_of_birth > 2023:
-        print("Year of birth must be between 1900 and 2023!")
-        year_of_birth = int(input("Year(YYYY): "))
-
-
-    date_of_birth = f"{day_of_birth}.{month_of_birth}.{year_of_birth}"
-    return date_of_birth
-
-
-
-
-def inputtingInitialDeposit():
-    initial_deposit_bool = input("\nDo you wish to make an initial deposit? (y/n): ").lower() 
-    if initial_deposit_bool == "y":
-        initial_deposit_amount = float(input("Enter your initial deposit amount: "))
-        print("Initial deposit amount successfully made.")
-    else:
-        initial_deposit_amount = 0
-
-    return initial_deposit_amount
